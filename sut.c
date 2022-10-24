bae0f82dedbb9a32a995e188050661a125867ff7
Miguel Bloggs <arrietam@tcd.ie>
2022-10-24 14:09:11 -0400

    Renamed main.c to sut.c

---
+++ sut.c
@@ -0,0 +1,37 @@
+#include "sut.h"
+#include "queue.h"
+
+void sut_init() {
+
+}
+
+bool sut_create(sut_task_f fn) {
+    return false;
+}
+
+void sut_yield() {
+
+}
+
+void sut_exit() {
+
+}
+
+void sut_open(char *dest, int port) {
+
+}
+
+void sut_write(char *buf, int size) {
+}
+
+void sut_close() {
+
+}
+
+char *sut_read() {
+    return NULL;
+}
+
+void sut_shutdown() {
+
+}
