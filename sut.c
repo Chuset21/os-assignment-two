707166d6ab635c526b3ff70ec22dbee9add0b95b
chuset <miguelarrieta02@gmail.com>
2022-10-24 21:04:13 +0000

    Started debugging segfault

--- sut.c
+++ sut.c
@@ -4,10 +4,9 @@
 #include "sut.h"
 #include "queue.h"
 
-pthread_t *c_exec;
-pthread_t *i_exec;
+pthread_t *c_exec, *i_exec;
 struct queue q;
-ucontext_t *c_exec_execute_context;
+ucontext_t *c_exec_context;
 
 #define STACK_SIZE (1024*1024)
 
@@ -18,7 +17,7 @@ _Noreturn void *c_exec_execute(__attribute__((unused)) void *arg) {
             usleep(100);
         } else {
             const ucontext_t *const ucontext = (ucontext_t *) pop;
-            swapcontext(c_exec_execute_context, ucontext);
+            swapcontext(c_exec_context, ucontext);
         }
     }
 }
@@ -26,7 +25,9 @@ _Noreturn void *c_exec_execute(__attribute__((unused)) void *arg) {
 void sut_init() {
     q = queue_create();
     queue_init(&q);
-    c_exec_execute_context = (ucontext_t *) malloc(sizeof(ucontext_t));
+    c_exec_context = (ucontext_t *) malloc(sizeof(ucontext_t));
+    // unnecessary
+    getcontext(c_exec_context);
 
     c_exec = (pthread_t *) malloc(sizeof(pthread_t));
     i_exec = (pthread_t *) malloc(sizeof(pthread_t));
@@ -50,6 +51,9 @@ bool sut_create(sut_task_f fn) {
     if (ucontext == NULL) {
         return false;
     }
+    if (getcontext(ucontext) < 0) {
+        return false;
+    }
 
     char *const uc_s = (char *) malloc(sizeof(char) * (STACK_SIZE));
     if (uc_s == NULL) {
@@ -59,7 +63,7 @@ bool sut_create(sut_task_f fn) {
     ucontext->uc_stack.ss_sp = uc_s;
     ucontext->uc_stack.ss_size = sizeof(char) * (STACK_SIZE);
     ucontext->uc_stack.ss_flags = 0;
-    ucontext->uc_link = c_exec_execute_context;
+    ucontext->uc_link = c_exec_context;
     makecontext(ucontext, fn, 0);
 
     return add_context_to_queue(ucontext);
@@ -68,11 +72,11 @@ bool sut_create(sut_task_f fn) {
 void sut_yield() {
     ucontext_t *const ucontext = (ucontext_t *) malloc(sizeof(ucontext_t));
     add_context_to_queue(ucontext);
-    swapcontext(ucontext, c_exec_execute_context);
+    swapcontext(ucontext, c_exec_context);
 }
 
 void sut_exit() {
-    setcontext(c_exec_execute_context);
+    setcontext(c_exec_context);
 }
 
 int sut_open(char *file_name) {
@@ -92,8 +96,8 @@ char *sut_read(int fd, char *buf, int size) {
 }
 
 void sut_shutdown() {
-    pthread_join(*c_exec, 0);
+    pthread_join(*c_exec, NULL);
     free(c_exec);
-    pthread_join(*i_exec, 0);
+    pthread_join(*i_exec, NULL);
     free(i_exec);
 }
